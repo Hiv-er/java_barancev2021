3299cc959a9587b212ef834ded48783d48b58e0a
Ilya <hiv-90@yandex.ru>
2021-04-14 10:53:13 +0600

    task 3

---
+++ sandbox/src/test/java/PointTests.java
@@ -0,0 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void pointLengthTest() {
+        Point p1 = new Point(8, -1);
+        Point p2 = new Point(4, 2);
+
+        Assert.assertEquals(p1.distance(p1, p2), 5.0);
+    }
+
+    @Test
+    public void pointLengthTest2() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(10, 4);
+
+        Assert.assertEquals(p1.distance(p1, p2), 10.770329614269007);
+    }
+
+    @Test
+    public void pointLengthTest3() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 0);
+
+        Assert.assertEquals(p1.distance(p1, p2), 0);
+    }
+}
