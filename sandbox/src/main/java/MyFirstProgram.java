ad1c4185233a46fe87f64c1919498b0c8d00f6cc
Ilya <hiv-90@yandex.ru>
2021-04-14 10:13:06 +0600

    task 2

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,16 @@
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(-3, 0);
+
+        System.out.println(distance(p1, p2));
+
+        System.out.println(p1.distance(p1, p2));
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
 
 }
